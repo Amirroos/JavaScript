7e7563de06c2b271d36aac1871ce1d1016d1498d
AMIR <golkhoshbooa@gmail.com>
2024-01-23 15:47:17 +0330

    NaN

--- 12/script.js
+++ 12/script.js
@@ -2,3 +2,8 @@ var firstName = "amir";
 var num1 = 1 ;
 alert(firstName - num1); //NaN
 alert (typeof NaN); //number 
+var test = Boolean(NaN); 
+alert(typeof test); //Boolean
+alert("12"+4); //124
+alert("12"+"4");//124
+alert("12"/4); //3
